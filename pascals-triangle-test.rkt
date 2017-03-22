614e0fa578158859da0b2bd1bfc045d3a2c1fe14
Cory Mickelson <cmickelson@skyslope.com>
2017-03-22 16:04:22 -0700

    one more test

--- pascals-triangle-test.rkt
+++ pascals-triangle-test.rkt
@@ -11,6 +11,7 @@
 
      (test-equal? "pascals triangle level 1" (pascals-triangle 1) '(1))
      (test-equal? "pascals triangle level 5" (pascals-triangle 5) '(1 4 6 4 1))
-     (test-equal? "pascals triangle level 6" (pascals-triangle 6) '(1 5 10 10 5 1))))
+     (test-equal? "pascals triangle level 6" (pascals-triangle 6) '(1 5 10 10 5 1))
+     (test-equal? "pascals triangle level 100[10]" (list-ref (pascals-triangle 100) 10)  15579278510796)))
 
   (run-tests suite))
